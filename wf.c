751050862734328e77b085a126fbd617fd899236
Demur Rumed <gunkmute@gmail.com>
2011-11-30 17:57:57 +0000

    Optimized

--- wf.c
+++ wf.c
@@ -15,111 +15,74 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <stdint.h>
-#define OOXOO 12
-#define OOOXX 11
-#define OOOOX 10
-#define OXOXO 9
-#define XOOOO 8
-#define XOOOX 7
-#define XOOXO 6
-#define XOOXX 5
-#define XOXOX 4
-#define XXOOX 3
-#define XXOXX 2
-#define XXXXO 1
+#define XOOOO 3
+#define OOOOX 2
+#define XOOOX 1
 #define XXXXX 0
-#define A(a,b,c,d,e) a|b<<4,c|d<<4,e
-#define B(a,b,c,d,e) |a<<4,b|c<<4,d|e<<4,
+#define A(a,b,c,d,e) a|b<<2|c<<4|d<<6,e
+#define B(a,b,c,d,e) |a<<2|b<<4|c<<6,d|e<<2
+#define C(a,b,c,d,e) |a<<4|b<<6,c|d<<2|e<<4
+#define D(a,b,c,d,e) |a<<6,b|c<<2|d<<4|e<<6,
 int T;
-static const char win[]={
-	2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,
-	2,2,2,2,2,3,3,2,3,3,2,2,2,2,2,2,
-	2,2,2,2,2,3,3,2,3,3,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,
-	2,2,2,2,2,3,2,2,2,3,2,2,2,2,2,2,
-	2,2,2,2,2,2,3,2,3,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,3,2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,
-	2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,
-};
-static const uint8_t abc[]={
+const uint8_t abc[]={
 	A(
-	XXXXX,
-	XOOOX,
-	XOXOX,
-	XOOOX,
-	XXXXX)
+	OOOOX,
+	OOOOX,
+	OOOOX,
+	OOOOX,
+	OOOOX)
 	B(
-	OOXOO,
-	OOXOO,
-	OOXOO,
-	OOXOO,
-	OOXOO)
-	A(
 	XXXXX,
 	OOOOX,
 	XXXXX,
 	XOOOO,
 	XXXXX)
-	B(
+	C(
 	XXXXX,
 	OOOOX,
 	XXXXX,
 	OOOOX,
 	XXXXX)
-	A(
+	D(
 	XOOOX,
 	XOOOX,
 	XXXXX,
 	OOOOX,
 	OOOOX)
-	B(
+	A(
 	XXXXX,
 	XOOOO,
 	XXXXX,
 	OOOOX,
 	XXXXX)
-	A(
+	B(
 	XOOOO,
 	XOOOO,
 	XXXXX,
 	XOOOX,
 	XXXXX)
-	B(
+	C(
 	XXXXX,
 	OOOOX,
 	OOOOX,
 	OOOOX,
 	OOOOX)
-	A(
+	D(
 	XXXXX,
 	XOOOX,
 	XXXXX,
 	XOOOX,
 	XXXXX)
-	B(
+	A(
 	XXXXX,
 	XOOOX,
 	XXXXX,
 	OOOOX,
 	OOOOX)
 };
-static void tfChar(int x,int y,char c){
-	for(int j=0;j<5;j++)
-		for(int i=0;i<5;i++)
-			if((0x4c41418a7359edff>>5*(abc[c*5+j>>1]>>(((c^j)&1)<<2)&15))&1<<i)glVertex2i(x+i,y+j);
-}
 #define case(x) break;case x:;
-unsigned char F[256];
-int lose;
+char F[256];
+int lose=1;
 void makefield(){
 	int m=0;
 	memset(F,0,256);
@@ -145,22 +108,18 @@ int getdot(int x,int y){
 	return getxy(x-1,y-1)+getxy(x-1,y)+getxy(x-1,y+1)+getxy(x,y-1)+getxy(x,y+1)+getxy(x+1,y-1)+getxy(x+1,y)+getxy(x+1,y+1);
 }
 void allzero(int x,int y){
-	x&=15;
-	y&=15;
 	int z=x|y<<4;
 	if(F[z]&2)return;
 	F[z]|=2;
-	for(int xx=x-1;xx<x+2;xx++)
-		for(int yy=y-1;yy<y+2;yy++){
-			int zz=(xx&15)|(yy&15)<<4;
-			if(!getdot(xx,yy))
-				allzero(xx,yy);
-			else F[zz]|=2;
+	for(int xx=-1;xx<2;xx++)
+		for(int yy=-1;yy<2;yy++){
+			int X=x+xx&15,Y=y+yy&15;
+			if(!getdot(X,Y))
+				allzero(X,Y);
+			else F[X|Y<<4]|=2;
 		}
 }
 int click(int x,int y){
-	x&=15;
-	y&=15;
 	int q=x|y<<4;
 	if(lose){
 		F[q]=1;
@@ -168,12 +127,16 @@ int click(int x,int y){
 		F[q]=0;
 		lose=0;
 		T=0;
-	}else if(F[q]==1)
+		if(!getdot(x,y))
+			allzero(x,y);
+	}else if(F[q]==1){
+		printf("Lost %d\n",T*3/100);
 		lose=1;
-	else if(!getdot(x,y))
+	}else if(!getdot(x,y))
 		allzero(x,y);
 	F[q]|=2;
 }
+const unsigned char col[][3]={{255,255,255},{255,255,0},{0,0,255},{255,0,0},{255,192,200}};
 int main(int argc,char**argv){
 	#ifdef SDL
 	if(SDL_Init(SDL_INIT_VIDEO)==-1){
@@ -192,8 +155,6 @@ int main(int argc,char**argv){
 	#endif
 	srand(time(0));
 	glOrtho(0,256,256,0,1,-1);
-	glColor3ub(255,255,255);
-	makefield();
 	for(;;){
 		#ifdef GLX
 		glXSwapBuffers(dpy,Wdo);
@@ -214,14 +175,15 @@ int main(int argc,char**argv){
 					if(!(F[q]&2))
 						F[q]^=4;
 				}else if(EV(button.button)==2){
-					printf("%d %d %d\n",getmark(x,y),getdot(x,y),F[q]);
 					if(getmark(x,y)==getdot(x,y)&&F[q]==2){
 						for(int xx=-1;xx<2;xx++)
-							for(int yy=-1;yy<2;yy++)
-								if(!(F[x+xx&15|(y+yy&15)<<4]&4)){
-									click(x+xx,y+yy);
+							for(int yy=-1;yy<2;yy++){
+								int X=x+xx&15,Y=y+yy&15;
+								if(!(F[X|Y<<4]&4)){
+									click(X,Y);
 									if(lose)goto lost;
 								}
+							}
 					}
 				}else if(!(F[q]&4)){
 					click(x,y);
@@ -230,6 +192,7 @@ int main(int argc,char**argv){
 			}
 		}
 		lost:glClear(GL_COLOR_BUFFER_BIT);
+		glColor3ubv(col[0]);
 		glBegin(GL_LINES);
 		for(int x=16;x<256;x+=16){
 			glVertex2i(x,0);
@@ -243,32 +206,41 @@ int main(int argc,char**argv){
 			int x=z&15,y=z>>4;
 			on+=F[z]&2;
 			if(F[z]&4){
-				glColor3ub(255,192,203);
+				glColor3ubv(col[4]);
 				glRecti(x*16,y*16,x*16+16,y*16+16);
-				glColor3ub(255,255,255);
 			}else if(F[z]==2){
 				int dots = getdot(x,y);
 				if(!dots){
-					glColor3ub(0,0,255);
+					glColor3ubv(col[2]);
 					glRecti(x*16,y*16,x*16+16,y*16+16);
-					glColor3ub(255,255,255);
 				}else{
+					glColor3ubv(col[0]);
 					glBegin(GL_POINTS);
-					tfChar(x*16+5,y*16+5,dots);
+					for(int j=0;j<5;j++)
+						for(int i=0;i<5;i++){
+							int bit=dots*10+j*2-10;
+							if((49727>>5*((abc[bit>>3]>>(bit&7))&3))&1<<i)glVertex2i(x*16+i+5,y*16+j+5);
+						}
 					glEnd();
 				}
 			}else if(F[z]&1){
 				if(!lose&&F[z]==1)continue;
-				glColor3ub(255,F[z]==1?255:0,0);
-				glRecti(x<<4,y<<4,x<<4|15,y<<4|15);
-				glColor3ub(255,255,255);
+				glColor3ubv(col[F[z]]);
+				glRecti(x*16,y*16,x*16+16,y*16+16);
 			}
 		}
-		printf("%d\n",on);
 		if(on==432){
-			printf("Won %d",T*3/100);
+			printf("Won %d\n",T*3/100);
 			lose=1;
-			memcpy(F,win,256);
+			memset(F,2,256);
+			memset(F+70,3,2);
+			memset(F+86,3,2);
+			memset(F+73,3,2);
+			memset(F+89,3,2);
+			int j=101;
+			for(int i=0;i<5;i++){
+				F[j+=i<3?17:-15]=3;
+			}
 		}
 		T++;
 		#ifdef GLX
